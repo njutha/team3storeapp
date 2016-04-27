2f756d68be808797b7f320b2ccb191835b6df563
iDoodly <Sahil@Sahils-MacBook-Pro.local>
2016-04-27 09:56:05 -0400

    Remove cart functionality

--- app/site/controllers/shop.ctrl.js
+++ app/site/controllers/shop.ctrl.js
@@ -16,17 +16,12 @@
 		shopVm.cart = productSrv.cart;
 		shopVm.orders;
 		shopVm.addToCart = productSrv.addToCart;
-
-		
 		
 		// console.log(product.data.product);
 
 		shopVm.viewProduct = function(product) {
 			$state.go('view-product',{productId: product.id});
 		};
-
-
-		
 
 		//watch for any changes to model data
 		$scope.$watch(function(){
@@ -50,6 +45,18 @@
 			console.log("this is the cart")
 			console.log(shopVm.cart)
 		}
+
+		shopVm.removeFromCart = function(productId){
+			for(var i=0; i < shopVm.cart.length; i++){
+				console.log(shopVm.cart[i])
+				console.log(productId)
+				if(shopVm.cart[i] == productId){
+					shopVm.cart.splice(i,1);
+					i--;
+				}
+			}
+		}
+
 	}
 })();
 
